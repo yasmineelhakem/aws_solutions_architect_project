5557493c4877c5583c4b39e956bffcbea1c95e58
yasmine <yasmine.elhakem@insat.ucar.tn>
2025-09-26 17:04:01 +0100

    download the image from S3 bucket

--- image_processor/app.py
+++ image_processor/app.py
@@ -1,17 +1,33 @@
 import json
-
+import boto3
 # import requests
 
+s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
 
-    print("Template test successful!")
-    print(f"Event received: {json.dumps(event, indent=2)}")
+    record = event['Records'][0] # Get the 1st record
+    bucket = record['s3']['bucket']['name'] # Get bucket name
+    key = record['s3']['object']['key'] # get the file name of the image
+
+    # the file should be an image
+    if not key.lower().endswith(('.jpg', '.jpeg', '.png')):
+        return {
+            'statusCode': 400,
+            'body': json.dumps({'error': 'Not an image file'})
+        }
+
+    # Download the image from the S3 bucket
+    response = s3.get_object(Bucket=bucket, Key=key)
+    image_data = response['Body'].read()
 
     return {
         'statusCode': 200,
         'body': json.dumps({
-            'message': 'Template test passed!',
-            'event_received': event
+            'message': 'S3 event received and image downloaded ',
+            'bucket': bucket,
+            'key': key,
+            'size': len(image_data)
+
         }),
     }
