273cc247e202e237539f812247666255a2f12af3
yasmine <yasmine.elhakem@insat.ucar.tn>
2025-09-26 17:56:59 +0100

    process image

--- image_processor/app.py
+++ image_processor/app.py
@@ -1,6 +1,7 @@
 import json
 import boto3
-# import requests
+from PIL import Image
+import io
 
 s3 = boto3.client('s3')
 
@@ -21,13 +22,24 @@ def lambda_handler(event, context):
     response = s3.get_object(Bucket=bucket, Key=key)
     image_data = response['Body'].read()
 
+    image = Image.open(io.BytesIO(image_data)) # Open image with Pillow
+    image.thumbnail((800, 800)) # Resize image
+    output_buffer = io.BytesIO()
+    image.save(output_buffer, format=image.format) # Save resized image to bytes
+    output_buffer.seek(0) # Reset buffer position
+
+    
     return {
         'statusCode': 200,
         'body': json.dumps({
             'message': 'S3 event received and image downloaded ',
             'bucket': bucket,
             'key': key,
-            'size': len(image_data)
+            'size': len(image_data),
+            'image_format': image.format,
+            'dimensions': f"{image.width}x{image.height}",
+            'processed_size': len(output_buffer.getvalue()),
+            'new_dimensions': f"{image.width}x{image.height}"
 
         }),
     }
