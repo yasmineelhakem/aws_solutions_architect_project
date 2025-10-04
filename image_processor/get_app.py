7d1f98f469fbc5dc234db1beb0ce15c5872b5f16
yasmine <yasmine.elhakem@insat.ucar.tn>
2025-10-04 15:48:07 +0100

    added api gateway

---
+++ image_processor/get_app.py
@@ -0,0 +1,19 @@
+import json, boto3, os
+
+def lambda_handler(event, context):
+    s3 = boto3.client("s3", endpoint_url=os.environ.get("AWS_ENDPOINT_URL"))
+    bucket = os.environ["PROCESSED_BUCKET"]
+    key = event["pathParameters"]["key"] # reads the key from api path
+
+    # generates presigned url for the user to download the image
+    url = s3.generate_presigned_url(
+        'get_object',
+        Params={'Bucket': bucket, 'Key': key},
+        ExpiresIn=300
+    )
+
+    return {
+        # Returns the JSON response to API Gateway which sends it to the client
+        "statusCode": 200,
+        "body": json.dumps({"download_url": url})
+    }
---
+++ image_processor/upload_app.py
@@ -0,0 +1,29 @@
+import json
+import boto3
+import os
+import time
+
+def lambda_handler(event, context):
+    s3 = boto3.client("s3", endpoint_url=os.environ.get("AWS_ENDPOINT_URL"))
+
+    bucket_name = os.environ.get("UPLOAD_BUCKET", "original-bucket") # reads s3 bucket name set in the env var from the template
+    file_name = f"user-upload-{int(time.time())}.png"  # creates unique file name
+
+    # Generate presigned URL (valid for 5 min) to let someone directly upload to s3 bucket
+    presigned_url = s3.generate_presigned_url(
+        'put_object',
+        Params={'Bucket': bucket_name,
+                 'Key': file_name, 
+                 'ContentType': 'image/png'
+        },
+        ExpiresIn=300
+    )
+
+    return {
+        # this is sent to the api gateway => client
+        "statusCode": 200,
+        "body": json.dumps({
+            "upload_url": presigned_url,
+            "file_key": file_name
+        })
+    }
